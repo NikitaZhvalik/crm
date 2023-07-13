2e6c69531ed57c2c0d30fde17fd7f0cb2dc4f07e
Nikita Zhvalik <130430335+NikitaZhvalik@users.noreply.github.com>
2023-07-13 15:23:02 +1000

    v.23 - изменение табуляции model

--- js/model.js
+++ js/model.js
@@ -1,68 +1,68 @@
 const formsAdd = loadRequests();
 
 const filter = {
-  products: 'all',
-  status: 'all',
+	products: 'all',
+	status: 'all',
 }
 
 function changeFilter(prop, value) {
-  filter[prop] = value;
-  return filter
+	filter[prop] = value;
+	return filter
 }
 
 function filterRequests(filter) {
-  let filterRequests;
+	let filterRequests;
 
-  //! фильтр по продукту
-  if (filter.products !== 'all') {
-    filterRequests = formsAdd.filter((request) => request.product === filter.products);
-  } else {
-    filterRequests = [...formsAdd];
-  }
+	//! фильтр по продукту
+	if (filter.products !== 'all') {
+		filterRequests = formsAdd.filter((request) => request.product === filter.products);
+	} else {
+		filterRequests = [...formsAdd];
+	}
 
-  filterRequestsStatus(filter);
+	filterRequestsStatus(filter);
 
-  return filterRequests;
+	return filterRequests;
 }
 
 function filterRequestsStatus(filter) {
-  let filterRequestsStatus;
+	let filterRequestsStatus;
 
-  //! фильтр по статусу
-  if (filter.status !== 'all') {
-    filterRequestsStatus = formsAdd.filter((request) => request.status === filter.status);
-  } else {
-    filterRequestsStatus = [...formsAdd];
-  }
+	//! фильтр по статусу
+	if (filter.status !== 'all') {
+		filterRequestsStatus = formsAdd.filter((request) => request.status === filter.status);
+	} else {
+		filterRequestsStatus = [...formsAdd];
+	}
 
-  return filterRequestsStatus;
+	return filterRequestsStatus;
 }
 
 function countNewRequest() {
-  const newRequests = formsAdd.filter((request) => request.status === 'new');
-  return newRequests.length;
+	const newRequests = formsAdd.filter((request) => request.status === 'new');
+	return newRequests.length;
 }
 
 function countAllRequest() {
-  return formsAdd.length;
+  	return formsAdd.length;
 }
 
 function countCompleteRequest() {
-  const completeRequest = formsAdd.filter((request) => request.status === 'complete');
-  return completeRequest.length;
+	const completeRequest = formsAdd.filter((request) => request.status === 'complete');
+	return completeRequest.length;
 }
 
 function countInWorkRequest() {
-  const inWorkRequest = formsAdd.filter((request) => request.status === 'inwork');
-  return inWorkRequest.length;
+	const inWorkRequest = formsAdd.filter((request) => request.status === 'inwork');
+	return inWorkRequest.length;
 }
 
 function createRecord(formData) {
     //! расчет id
     let id = 1;
     if (formsAdd.length > 0) {
-      const lastElement = formsAdd[formsAdd.length - 1];
-      id = lastElement.id + 1;
+		const lastElement = formsAdd[formsAdd.length - 1];
+		id = lastElement.id + 1;
     }
 
     //! работа с временем
@@ -71,47 +71,44 @@ function createRecord(formData) {
   
     //! формируем заявку
     const formAdd = {
-      id: id,
-      date: date,
-      dateTime: dateTime,
-      status: 'new', 
-      ...formData,
+		id: id,
+		date: date,
+		dateTime: dateTime,
+		status: 'new', 
+		...formData,
     };
 
     formsAdd.push(formAdd);
 
     saveRequests();
-
-    console.log(formsAdd);
 }
-console.log(formsAdd);
 
 function saveRequests(){
-  localStorage.setItem('formsAdd', JSON.stringify(formsAdd))
+ 	localStorage.setItem('formsAdd', JSON.stringify(formsAdd))
 }
 
 function loadRequests(){
-  return localStorage.getItem('formsAdd') ? JSON.parse(localStorage.getItem('formsAdd')) : [];
+  	return localStorage.getItem('formsAdd') ? JSON.parse(localStorage.getItem('formsAdd')) : [];
 }
 
 function getForms(){
-  return formsAdd
+  	return formsAdd
 }
 
 function getRequestById(id) {
-  const request = formsAdd.find((item) => item.id == id);
-  request.dateDate = new Date(request.date).toLocaleDateString();
-  return request;
+	const request = formsAdd.find((item) => item.id == id);
+	request.dateDate = new Date(request.date).toLocaleDateString();
+	return request;
 }
 
 function updateRequest(formData) {
-  const request = getRequestById(formData.get('id'));
-  request.name = formData.get('name');
-  request.email = formData.get('email');
-  request.phone = formData.get('phone');
-  request.product = formData.get('product');
-  request.status = formData.get('status');
-  saveRequests();
+	const request = getRequestById(formData.get('id'));
+	request.name = formData.get('name');
+	request.email = formData.get('email');
+	request.phone = formData.get('phone');
+	request.product = formData.get('product');
+	request.status = formData.get('status');
+	saveRequests();
 }
 
 export {formsAdd, changeFilter, filterRequests, createRecord, getForms, getRequestById, updateRequest, filterRequestsStatus, countNewRequest
